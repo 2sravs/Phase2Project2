340f9705678cb649e0f2dfbe3a3996c823a8d60b
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-06 23:54:17 +0530

    firstcommit

---
+++ Data/WebProject1DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIProject1.Models;
+
+namespace WebAPIProject1.Data
+{
+    public class WebProject1DbContext : DbContext
+    {
+        public WebProject1DbContext (DbContextOptions<WebProject1DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIProject1.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
---
+++ Models/DeptMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIProject1.Models
+{
+    public class DeptMaster
+    {
+        [Key]
+        public int DeptCode { get; set; }
+        public string DeptName { get; set; }
+        public virtual ICollection<EmpProfile> EmpProfiles { get; set; }
+    }
+    public class EmpProfile
+    {
+        [Key]
+        public int EmpCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public int DeptCode { get; set; }
+        
+    }
+}
